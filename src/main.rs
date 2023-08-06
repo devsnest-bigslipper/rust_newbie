5dc2271880d15f0ec134dd62d68ce39132aa9f52
devsnest-big <default-big>
2023-08-06 20:22:42 +0800

    modified main.rs and Cargo.toml. ran command prompt:cargo add rand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+use rand::Rng;
+
+
 fn main() {
     println!("Hello, world!");
     
@@ -31,7 +35,7 @@ fn main() {
     let arr: [i32;5] = [1,2,3,4,5];
     
     //turple
-    let tur: (i32,f64,u8) = (500,6.4,1);
+    let tur: (i32,f64,u8) = (-500,6.4,1);
 
     let (x,y,z) = tur;
     //same as following syntax:
@@ -39,7 +43,7 @@ fn main() {
     //let y:f64=tur.1;
     //let z:u8=tur.2;
     
-    //let (x: i32,y: f64,z: u8) = tur; not working?
+    //let (x: i32,y: f64,z: u8) = tur; why not working?
 
     println!("unsigned:{}, signed:{}, float:{}, letter:{}, is_true:{}",unsigned,signed,float,letter,is_true);
     println!("index:{}, length:{}",arr[0],arr.len());
@@ -52,7 +56,62 @@ fn main() {
     //^^^^^^^^^^^^^^
     
     another_function();
+    
+    //Put values into a function
+    //function_a( x:5, alphabet:'a' );
+    functionA( 5, 'a' );
+   
+    //function?
+    let x_tmp1: ()={
+       let mut y_tmp1: i32=5 ;
+       y_tmp1=y_tmp1+1;
+    };
+    println!("{:?}",x_tmp1);
 
+
+    //vvvvvvvvvvvvvvvvvv
+    //|| Control Flow ||
+    //^^^^^^^^^^^^^^^^^^
+
+    let x: i32 =3;
+    if x>5{
+        println!("x is greater than 5");
+    }
+    else if x<2{
+        println!("x is less than 2");
+    }
+    else{
+        println!("x is between 2 and 5");
+    }
+   
+    //vvvvvvvvvvvvvvvvvvv
+    //|| Random number ||
+    //^^^^^^^^^^^^^^^^^^^
+
+    println!("Guessing a number");
+    let secret_number:u32=rand::thread_rng().gen_range(1..=100);
+
+    loop{
+        println!("Please guess a number:");
+        let mut guess: String=String::new();
+        io::stdin() Stdin
+            .read_line(buf:(&mut guess)) Result(usize, Error)
+            .expect(msg:"failed to read the line");
+
+        let guess:u32=match guess.trim().parse(){
+            Ok(num: u32) => num,
+            Err(_)=>continue,
+        };
+        println!("Your guessed:{}".guess);
+        if guess>secret_number{
+            println!("too big!");
+        else if guess<secret_number {
+            println!("too small!");
+        }
+        else{
+            println!("correct!");
+            break;
+        }
 
 }
 
@@ -60,4 +119,8 @@ fn main() {
 
 fn another_function() {
     println!("another function");
+}
+
+fn functionA(x:i32,alphabet:char) {
+    println!("functionA x:{},alphabet:{}",x,alphabet);
 }
